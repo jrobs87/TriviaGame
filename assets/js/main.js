498ba38ed79fa9dbdd6f9cb42f50272252717a1d
John Robertson <tiktaalikdesign@gmail.com>
2019-03-26 00:27:25 -0400

    js rendering question block and answers.  will require additional divs to hold buttons, question text, and a way to append buttons to each quesion block

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,34 @@ var questionsArray = [
     {
         question: 'what is your home planet?',
         answer: 'earth',
-        choices: ['earth', 'mars', 'jupiter'],
+        choices: ['earth', 'mars', 'jupiter', 'venus'],
     },
-    { question: 'what is the fourth planet from the sun?', answer: 'mars' },
+
 ];
 
+// loops through questin=on objects to create a new question block
 for (i = 0; i < questionsArray.length; i++) {
-    var question = document.createElement('div'); // creates the question block
-    question.classList.add('questionBlock'); // assigns class name to question block
-    question.setAttribute("value", i); // sets value for question block to grab for clicks later
 
-    var content = document.createTextNode(questionsArray[i].question);
-    question.appendChild(content);
-    document.getElementById('app').append(question);
+    var questionBlock = document.createElement('div'); // creates the question block
+    questionBlock.classList.add('questionBlock'); // assigns class name to question block
+    questionBlock.setAttribute("id", i); // sets value for question block to grab for clicks later
 
+    var content = document.createTextNode(questionsArray[i].question); // create text node for the question
+    questionBlock.appendChild(content); // adds question text to div
+
+    document.getElementById('app').append(questionBlock); // appends question div to the app element
+
+    // loops thru each answer choice in our question object to create a button
+    for (a = 0; a < questionsArray[i].choices.length; a++) {
+
+        var choice = document.createElement('button'); //creates button element
+        choice.setAttribute("class", "buttonChoice"); // sets value for question block to grab for clicks later
+
+        var contentChoice = document.createTextNode(questionsArray[i].choices[a]); // creates answer button
+        choice.appendChild(contentChoice); // adds answer text to button
+
+        document.getElementById('0').appendChild(choice); // appends button to the question block by ID -  this will require making a new container first
+    };
 };
 
 
